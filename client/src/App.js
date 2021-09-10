2737e002a127963e70664e53f88841fbef5e970e
TaraMKelly <tara.kelly16@gmail.com>
2021-09-10 18:13:17 -0400

    implemented client side login/signup feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-
+import { useEffect, useState } from "react"
+import Login from "/Login"
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
+
   return (
     <div className="App">
       Bonjour
