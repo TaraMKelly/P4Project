5d122a9f187a2f1cf4b29e69178ecae06d8edcc4
Quinn Lonergan <qui99lo@gmail.com>
2021-09-13 14:09:21 -0400

    Fixed migrations and added seed file

--- db/migrate/20210913180124_create_user_drinks.rb
+++ db/migrate/20210913180124_create_user_drinks.rb
@@ -1,8 +1,8 @@
 class CreateUserDrinks < ActiveRecord::Migration[6.1]
   def change
     create_table :user_drinks do |t|
-      t.belongs_to :drinks, null: false, foreign_key: true
-      t.belongs_to :users, null: false, foreign_key: true
+      t.belongs_to :drink, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
