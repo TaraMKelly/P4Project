40be828a2d19634ba6bc62032fc9b79e401ea534
Quinn Lonergan <qui99lo@gmail.com>
2021-09-14 16:39:05 -0400

    Basic search functions

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,15 +1,32 @@
-
+import { useEffect, useState } from "react";
+import RenderSearchResults from "./RenderSearchResults";
+import { Button, Input } from "../styles";
 
 function SearchBar({ searchValue, setSearchValue }) {
+    const [newSearchValue, setNewSearchValue] = useState("")
+    const [searchObject, setSearchObject] = useState("")
 
     function handleSearch(event) {
-        setSearchValue(event.target.value)
+        setNewSearchValue(event.target.value)
     }
 
-    return (
-        <form>
-            <input type="text" name="searchBar" value={searchValue} placeholder="Search..." onChange={handleSearch}></input>
-        </form>
+    // function handleSearch(event) {
+    //     setSearchValue(event.target.value)
+    // }
+
+    const handleButtonSearch = (e) => {
+        e.preventDefault()
+        setSearchObject(newSearchValue)
+    }
+
+    return(
+        <div>
+            <form className = 'searchForm'>
+                <Input placeholder='Search for a cocktail' size="large" onChange={handleSearch}/>          
+                <Button type='submit' onClick={handleButtonSearch}>Search</Button>
+            </form>
+            <RenderSearchResults searchObject = {searchObject}/>
+        </div>
     )
 }
 export default SearchBar
