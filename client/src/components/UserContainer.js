b408feba3e6812fe995ad4d003bd30d6bc2423d0
Quinn Lonergan <qui99lo@gmail.com>
2021-10-20 16:16:22 -0500

    cleaned up a lil

--- client/src/components/UserContainer.js
+++ client/src/components/UserContainer.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import UserCard from './UserCard';
 import { Container } from 'semantic-ui-react'
-// import styled from "styled-components";
 
 
 function UserContainer({ liked, setLiked, setGetDrinkId, setUpdate, drinks, setDrinks }) {
@@ -18,23 +17,13 @@ function UserContainer({ liked, setLiked, setGetDrinkId, setUpdate, drinks, setD
       <Container> 
         <h1>{`Hello ${user.username}!`}</h1>
         <h3><b>Your Favorite Drinks:</b> </h3>
-        {/* <ContentBox> */}
           <UserCard user={user} liked={liked} setLiked={setLiked} setGetDrinkId={setGetDrinkId} setUpdate={setUpdate} drinks={drinks} setDrinks={setDrinks} />
-        {/* </ContentBox> */}
       </Container>
     </>
   );
 
 
 }
-// const ContentBox = styled.div`
-//   display: flex;
-//   gap: 0.25rem;
-//   padding: 0.25rem;
-//   flex-wrap: wrap;
-//   align-items: center;
-//   grid-area: content;
-//   justify-content: center;
-// `;
+
 
 export default UserContainer;
